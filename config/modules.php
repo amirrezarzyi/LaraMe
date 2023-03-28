d6baab3131dd2c9cd45cc332257cd69db786c3b6
Dev.Amirreza <dev.amirrezaa@gmail.com>
2023-03-28 09:06:23 +0330

    change module config

--- config/modules.php
+++ config/modules.php
@@ -26,29 +26,22 @@ return [
     */
 
     'stubs' => [
-        'enabled' => false,
-        'path' => base_path('vendor/nwidart/laravel-modules/src/Commands/stubs'),
-        'files' => [
+        'enabled' => true,
+        'path'    => base_path('stubs/amiza-stubs'),
+        'files'   => [
             'routes/web' => 'Routes/web.php',
             'routes/api' => 'Routes/api.php',
-            'views/index' => 'Resources/views/index.blade.php',
-            'views/master' => 'Resources/views/layouts/master.blade.php',
-            'scaffold/config' => 'Config/config.php',
-            'composer' => 'composer.json',
-            'assets/js/app' => 'Resources/assets/js/app.js',
-            'assets/sass/app' => 'Resources/assets/sass/app.scss',
-            'vite' => 'vite.config.js',
-            'package' => 'package.json',
+            'composer'   => 'composer.json',
         ],
         'replacements' => [
-            'routes/web' => ['LOWER_NAME', 'STUDLY_NAME'],
-            'routes/api' => ['LOWER_NAME'],
-            'vite' => ['LOWER_NAME'],
-            'json' => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE', 'PROVIDER_NAMESPACE'],
-            'views/index' => ['LOWER_NAME'],
-            'views/master' => ['LOWER_NAME', 'STUDLY_NAME'],
+            'routes/web'      => ['LOWER_NAME', 'STUDLY_NAME'],
+            'routes/api'      => ['LOWER_NAME'],
+            'vite'            => ['LOWER_NAME'],
+            'json'            => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE', 'PROVIDER_NAMESPACE'],
+            'views/index'     => ['LOWER_NAME'],
+            'views/master'    => ['LOWER_NAME', 'STUDLY_NAME'],
             'scaffold/config' => ['STUDLY_NAME'],
-            'composer' => [
+            'composer'        => [
                 'LOWER_NAME',
                 'STUDLY_NAME',
                 'VENDOR',
@@ -58,7 +51,7 @@ return [
                 'PROVIDER_NAMESPACE',
             ],
         ],
-        'gitkeep' => true,
+        'gitkeep' => false,
     ],
     'paths' => [
         /*
@@ -101,32 +94,32 @@ return [
         | Set the generate key to false to not generate that folder
         */
         'generator' => [
-            'config' => ['path' => 'Config', 'generate' => true],
-            'command' => ['path' => 'Console', 'generate' => true],
-            'migration' => ['path' => 'Database/Migrations', 'generate' => true],
-            'seeder' => ['path' => 'Database/Seeders', 'generate' => true],
-            'factory' => ['path' => 'Database/factories', 'generate' => true],
-            'model' => ['path' => 'Entities', 'generate' => true],
-            'routes' => ['path' => 'Routes', 'generate' => true],
-            'controller' => ['path' => 'Http/Controllers', 'generate' => true],
-            'filter' => ['path' => 'Http/Middleware', 'generate' => true],
-            'request' => ['path' => 'Http/Requests', 'generate' => true],
-            'provider' => ['path' => 'Providers', 'generate' => true],
-            'assets' => ['path' => 'Resources/assets', 'generate' => true],
-            'lang' => ['path' => 'Resources/lang', 'generate' => true],
-            'views' => ['path' => 'Resources/views', 'generate' => true],
-            'test' => ['path' => 'Tests/Unit', 'generate' => true],
-            'test-feature' => ['path' => 'Tests/Feature', 'generate' => true],
-            'repository' => ['path' => 'Repositories', 'generate' => false],
-            'event' => ['path' => 'Events', 'generate' => false],
-            'listener' => ['path' => 'Listeners', 'generate' => false],
-            'policies' => ['path' => 'Policies', 'generate' => false],
-            'rules' => ['path' => 'Rules', 'generate' => false],
-            'jobs' => ['path' => 'Jobs', 'generate' => false],
-            'emails' => ['path' => 'Emails', 'generate' => false],
-            'notifications' => ['path' => 'Notifications', 'generate' => false],
-            'resource' => ['path' => 'Transformers', 'generate' => false],
-            'component-view' => ['path' => 'Resources/views/components', 'generate' => false],
+            'config'          => ['path' => 'Config', 'generate' => false],
+            'command'         => ['path' => 'Console', 'generate' => false],
+            'migration'       => ['path' => 'Database/Migrations', 'generate' => false],
+            'seeder'          => ['path' => 'Database/Seeders', 'generate' => false],
+            'factory'         => ['path' => 'Database/factories', 'generate' => false],
+            'model'           => ['path' => 'Entities', 'generate' => false],
+            'routes'          => ['path' => 'Routes', 'generate' => false],
+            'controller'      => ['path' => 'Http/Controllers', 'generate' => false],
+            'filter'          => ['path' => 'Http/Middleware', 'generate' => false],
+            'request'         => ['path' => 'Http/Requests', 'generate' => false],
+            'provider'        => ['path' => 'Providers', 'generate' => true],
+            'assets'          => ['path' => 'Resources/assets', 'generate' => false],
+            'lang'            => ['path' => 'Lang', 'generate' => true],
+            'views'           => ['path' => 'Resources/views', 'generate' => false],
+            'test'            => ['path' => 'Tests/Unit', 'generate' => false],
+            'test-feature'    => ['path' => 'Tests/Feature', 'generate' => false],
+            'repository'      => ['path' => 'Repositories', 'generate' => false],
+            'event'           => ['path' => 'Events', 'generate' => false],
+            'listener'        => ['path' => 'Listeners', 'generate' => false],
+            'policies'        => ['path' => 'Policies', 'generate' => false],
+            'rules'           => ['path' => 'Rules', 'generate' => false],
+            'jobs'            => ['path' => 'Jobs', 'generate' => false],
+            'emails'          => ['path' => 'Emails', 'generate' => false],
+            'notifications'   => ['path' => 'Notifications', 'generate' => false],
+            'resource'        => ['path' => 'Transformers', 'generate' => false],
+            'component-view'  => ['path' => 'Resources/views/components', 'generate' => false],
             'component-class' => ['path' => 'View/Components', 'generate' => false],
         ],
     ],
@@ -214,10 +207,10 @@ return [
     */
 
     'composer' => [
-        'vendor' => 'nwidart',
+        'vendor' => 'DeAmiza',
         'author' => [
-            'name' => 'Nicolas Widart',
-            'email' => 'n.widart@gmail.com',
+            'name' => 'Amirreza',
+            'email' => 'DevAmirrezaa@gmail.com',
         ],
         'composer-output' => false,
     ],
@@ -265,10 +258,10 @@ return [
     | The file activator will store the activation status in storage/installed_modules
     */
     'activators' => [
-        'file' => [
-            'class' => FileActivator::class,
-            'statuses-file' => base_path('modules_statuses.json'),
-            'cache-key' => 'activator.installed',
+        'file'   => [
+            'class'          => FileActivator::class,
+            'statuses-file'  => base_path('modules_statuses.json'),
+            'cache-key'      => 'activator.installed',
             'cache-lifetime' => 604800,
         ],
     ],
